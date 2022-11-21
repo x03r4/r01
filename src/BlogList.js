d9b311e9e4960b711128957ed8570060ed5ff572
Daniel Kobrzycki <daniel.kobrzycki@gmail.com>
2022-11-21 18:10:59 +0100

    added links on homepage

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,5 +1,6 @@
-const BlogList = (props) => {
+import { Link } from "react-router-dom";
 
+const BlogList = (props) => {
   let { blogs, title } = props;
 
   return (
@@ -7,9 +8,10 @@ const BlogList = (props) => {
       <h2>{title}</h2>
       {blogs.map((blog) => (
         <div className="blog-preview" key={blog.id}>
-          <h2>{blog.title}</h2>
-          <p>Written by {blog.author}</p>
-      
+          <Link to={`blogs/${blog.id}`}>
+            <h2>{blog.title}</h2>
+            <p>Written by {blog.author}</p>
+          </Link>
         </div>
       ))}
     </div>
