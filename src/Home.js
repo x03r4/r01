5169555fba56d5efb7eb7e97e358564b22aa472f
Daniel Kobrzycki <daniel.kobrzycki@gmail.com>
2022-11-21 16:35:39 +0100

    added handling error

--- src/Home.js
+++ src/Home.js
@@ -4,6 +4,8 @@ import BlogList from "./BlogList";
 const Home = () => {
 
     const [blogs, setBlogs] = useState(null)
+    const [isPending, setIsPending] = useState(true)
+    const [error, setError] = useState(null)
 
     const handleDelete = (id) => {
         const newBlogs = blogs.filter(blog => blog.id !== id)
@@ -13,16 +15,26 @@ const Home = () => {
     useEffect(() => {
         fetch('http://localhost:8000/blogs')
             .then(res => {
+                if(!res.ok) {
+                    throw Error('could not fetch the data for that resource')
+                }
                 return res.json()
             })
             .then(data => {
-                console.log(data)
                 setBlogs(data)
+                setIsPending(false)
+                setError(null)
+            })
+            .catch((err) => {
+                setError(err.message)
+                setIsPending(false)
             })
     },[])
 
     return ( 
         <div className="home">
+            { error && <div>{ error }</div> }
+            { isPending && <div>Loading...</div> }
             {blogs && <BlogList blogs={blogs} title="All blogs" handleDelete={handleDelete}/>}
         </div>
      );
