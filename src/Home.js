bff0d31247a89e7e36191e7b29d1279288182e0f
Daniel Kobrzycki <daniel.kobrzycki@gmail.com>
2022-11-21 14:36:09 +0100

    setting reusable BlogList component

--- src/Home.js
+++ src/Home.js
@@ -1,27 +1,17 @@
 import { useState } from "react"
+import BlogList from "./BlogList";
 
 const Home = () => {
-    const [name, setName] = useState('mario');
-    const handleClick = () => {
-     setName('luigi')
-    }
 
-    let [count, setCount] = useState(0);
-    const counting = () => {
-        setCount(
-            count = count + 1
-            )
-        }
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+    ])
 
     return ( 
         <div className="home">
-            <h2>Homepage</h2>
-            <p>{ name }</p>
-            <button onClick={handleClick}>Click me</button>
-
-            <button onClick={counting}>klikaj dziwko</button>
-            <h5>kliknąłeś { count } razy, dziwko.</h5>
-
+            <BlogList blogs={blogs} title="tytuł komponenta blogów"/>
         </div>
      );
 }
