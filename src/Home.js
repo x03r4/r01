b5e5e7ddf4f91c75ac9c9ad3448cbd16d480c962
Daniel Kobrzycki <daniel.kobrzycki@gmail.com>
2022-11-21 15:18:11 +0100

    added delete blog feature

--- src/Home.js
+++ src/Home.js
@@ -9,9 +9,15 @@ const Home = () => {
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
     ])
 
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter(blog => blog.id !== id)
+        setBlogs(newBlogs)
+    }
+
     return ( 
         <div className="home">
-            <BlogList blogs={blogs} title="tytuł komponenta blogów"/>
+            <BlogList blogs={blogs} title="All blogs" handleDelete={handleDelete}/>
+            {/* <BlogList blogs={blogs.filter((blog)=>blog.author === 'mario')} title="Mario's blogs"/> */}
         </div>
      );
 }
