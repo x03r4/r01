3783871a51fbf9c5a162ecf10c531dc76dd4504f
Daniel Kobrzycki <daniel.kobrzycki@gmail.com>
2022-11-21 20:22:55 +0100

    now U can add blog to your site

--- src/Create.js
+++ src/Create.js
@@ -5,11 +5,22 @@ const Create = () => {
     const [title, setTitle] = useState('')
     const [body, setBody] = useState('')
     const [author, setAuthor] = useState('mario')
+    const [isPending, setIsPending] = useState(false)
 
     const handleSubmit = (e) => {
         e.preventDefault()
         const blog = { title, body, author }
-        console.log(blog)
+
+        setIsPending(true)
+
+        fetch('http://localhost:8000/blogs', {
+            method: 'POST',
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(blog)
+        }).then(() => {
+            console.log('new blog added')
+            setIsPending(false)
+        })
     }
 
     return ( 
@@ -37,7 +48,8 @@ const Create = () => {
                     <option value="mario">Mario</option>
                     <option value="yoshi">Yoshi</option>
                 </select>
-                <button>Add Blog</button>
+                { !isPending && <button>Add Blog</button> }
+                { isPending && <button disabled>Adding Blog...</button> }
                 <p>{ title }</p>
                 <p>{ body }</p>
                 <p>{ author }</p>
