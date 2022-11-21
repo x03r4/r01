e4f6b93fe5feffc397b062ffdafdd2882171afb5
Daniel Kobrzycki <daniel.kobrzycki@gmail.com>
2022-11-21 19:39:21 +0100

    created and styled add new blog form

--- src/Create.js
+++ src/Create.js
@@ -2,6 +2,22 @@ const Create = () => {
     return ( 
         <div className="create">
             <h2>Add a new blog</h2>
+            <form>
+                <label>Blog title:</label>
+                <input 
+                    type="text"
+                    required 
+                />
+                <label>Blog body:</label>
+                <textarea
+                required
+                ></textarea>
+                <label>Blog author:</label>
+                <select>
+                    <option value="mario">Mario</option>
+                    <option value="yoshi">Yoshi</option>
+                </select>
+            </form>
         </div>
      );
 }
