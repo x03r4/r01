bb4a7d4f8bd071f86beed712acf7c461f62333b0
Daniel Kobrzycki <daniel.kobrzycki@gmail.com>
2022-11-21 13:58:29 +0100

    basic useState hooks

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,27 @@
+import { useState } from "react"
+
 const Home = () => {
+    const [name, setName] = useState('mario');
+    const handleClick = () => {
+     setName('luigi')
+    }
+
+    let [count, setCount] = useState(0);
+    const counting = () => {
+        setCount(
+            count = count + 1
+            )
+        }
+
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <p>{ name }</p>
+            <button onClick={handleClick}>Click me</button>
+
+            <button onClick={counting}>klikaj dziwko</button>
+            <h5>kliknąłeś { count } razy, dziwko.</h5>
+
         </div>
      );
 }
